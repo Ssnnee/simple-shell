3f97e18aa0059567ae785f58d10782078daf5268
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-05-19 14:25:41 -0700

    Simple shell 0.4.1 => done :)

--- execute_command.c
+++ execute_command.c
@@ -30,7 +30,14 @@ int execute(char *app_name, char **args, char **envp)
 	{
 		/*Handle exit cmd */
 		if (_strcmp(args[0], "exit"))
-			exit(0);
+		{
+			if (args[1] == NULL)
+				exit(0);
+			else
+			{
+				exit(atoi(args[1]));
+			}
+		}
 		cmd = get_full_path(args[0]);
 		/*Use fork to come back on the shell after execution of cmd*/
 		child_pid = fork();
@@ -38,20 +45,7 @@ int execute(char *app_name, char **args, char **envp)
 		{
 			if (execve(cmd, args, envp) == -1)
 			{
-				char *error_msg, *not_found_msg;
-				int i;
-
-				error_msg = app_name;
-				for (i = 0; error_msg[i] != '\0'; i++)
-					_putchar(error_msg[i]);
-				_putchar(':');
-				_putchar(' ');
-				for (i = 0; args[0][i] != '\0'; i++)
-					_putchar(args[0][i]);
-
-				not_found_msg = ": not found\n";
-				for (i = 0; not_found_msg[i] != '\0'; i++)
-					_putchar(not_found_msg[i]);
+				perror(app_name);
 				exit(EXIT_FAILURE);
 			}
 		}
