5b108b4cf954013f4093d50b4dd0cce57eaa10b3
Samuel Nandi <nandisamuelsne@gmail.com>
2023-05-22 12:38:51 -0700

    Atoi

--- atoi.c
+++ atoi.c
@@ -11,4 +11,20 @@ int _atoi(char *s)
 	int sign, num i;
 
 	sign = 1, num = 0, i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = num * 10 + (s[i] - '0');
+			if (!(s[i + 1] >= '0' && s[i + 1] <= '9'))
+			break;
+		}
+		else if (num > 0)
+			break;
+		i++;
+	}
+	return (num * sign);
 }
