87ec998750ce5c5e41a5d624f7376831050cc469
Samuel Nandi <nandisamuelsne@gmail.com>
2023-05-26 08:19:11 -0700

    The last task

--- hsh.c
+++ hsh.c
@@ -1,5 +1,12 @@
 #include "hsh.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/time.h>
 /**
  * main - The main function of the shell program.
  * @argc: The number of arguments passed to the program.
@@ -9,14 +16,49 @@
  * Return: The exit status of the shell program.
  */
 int main(int argc, char **argv, char **envp)
-{
-	/* Suppress unused parameter warnings */
-	(void)argc;
-	(void)argv;
+{ int fd;
+	char buf[MAX_LINE];
+	ssize_t nread, i;
+	char *line, *new_line;
+	size_t line_size, j;
+	char **args;
 
-	/* Run the shell loop */
-	return (shell_loop(argv[0], envp));
+	line = NULL, line_size = 0;
+	if (argc > 1)
+	{
+		fd = open(argv[1], O_RDONLY);
+		if (fd == -1)
+		{ perror(argv[0]);
+			exit(EXIT_FAILURE);
+		}
+		while ((nread = read(fd, buf, MAX_LINE)) > 0)
+		{
+			for (i = 0; i < nread; i++)
+			{
+				if (buf[i] == '\n')
+				{ args = parse_line(line);
+					execute(argv[0], args, envp);
+					free(args);
+					free(line);
+					line = NULL;
+					line_size = 0;
+				}
+				else
+				{ new_line = malloc(line_size + 2);
+					for (j = 0; j < line_size; j++)
+						new_line[j] = line[j];
+					new_line[line_size++] = buf[i];
+					new_line[line_size] = '\0';
+					free(line);
+					line = new_line;
+				}
+			}
+		} close(fd);
+		free(line);
+		exit(EXIT_SUCCESS);
+	} return (shell_loop(argv[0], envp));
 }
+
 /**
  * _putchar - This function writes a character to stdout.
  * @c: The character to be written.
